b6a493482d250682780bdc7544582968d2619ed1
DVD <zjdavid.2003@gmail.com>
2026-03-03 13:27:02 -0500

    Switch back to normal trainer for eval

--- examples/tactile/eval_qwen3_vl-8b-megatron_reasoning_date_single_view.sh
+++ examples/tactile/eval_qwen3_vl-8b-megatron_reasoning_date_single_view.sh
@@ -1,22 +1,12 @@
 set -x
-ENGINE=${1:-vllm}
-export CUDA_DEVICE_MAX_CONNECTIONS=1
-export VLLM_ALLREDUCE_USE_SYMM_MEM=0
-export CUDA_LAUNCH_BLOCKING=0
 
 # Evaluate reasoning RL checkpoint on ablation test set — split: date, single view
-
-GEN_TP=${GEN_TP:-4}
-CP=${CP:-2}
-TP=${TP:-2}
-PP=${PP:-1}
 
 MODEL_PATH="/scratch/dvdai/qwen3vl_dapo_reasoning_split_date_2"
 
 test_path="$HOME/scratch/raofu/3DHaptic/annotation_verl_split_date_test_single_view.json"
 
-CUDA_VISIBLE_DEVICES=0,3,4,7 python3 -m verl.trainer.main_ppo --config-path=config \
-    --config-name='ppo_megatron_trainer.yaml'\
+CUDA_VISIBLE_DEVICES=4,5,6,7 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files="$test_path" \
     data.val_files="$test_path" \
@@ -27,45 +17,24 @@ CUDA_VISIBLE_DEVICES=0,3,4,7 python3 -m verl.trainer.main_ppo --config-path=conf
     data.filter_overlong_prompts=True \
     data.truncation='left' \
     actor_rollout_ref.model.path="$MODEL_PATH" \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.actor.ppo_mini_batch_size=64 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
-    actor_rollout_ref.actor.megatron.pipeline_model_parallel_size=$PP \
-    actor_rollout_ref.actor.megatron.tensor_model_parallel_size=$TP \
-    actor_rollout_ref.actor.megatron.context_parallel_size=$CP \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.01 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.actor.entropy_coeff=0 \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=4 \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=$GEN_TP \
-    actor_rollout_ref.actor.use_dynamic_bsz=True \
-    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=12288 \
-    actor_rollout_ref.ref.log_prob_use_dynamic_bsz=True \
-    actor_rollout_ref.ref.log_prob_max_token_len_per_gpu=12288 \
-    actor_rollout_ref.rollout.log_prob_use_dynamic_bsz=True \
-    actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=12288 \
-    actor_rollout_ref.rollout.name=$ENGINE \
+    actor_rollout_ref.actor.fsdp_config.param_offload=False \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=2 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
+    actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
     actor_rollout_ref.rollout.n=5 \
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=4 \
-    actor_rollout_ref.actor.megatron.use_mbridge=True \
-    actor_rollout_ref.actor.megatron.param_offload=True \
-    actor_rollout_ref.actor.megatron.optimizer_offload=True \
-    actor_rollout_ref.actor.megatron.grad_offload=True \
-    actor_rollout_ref.ref.megatron.param_offload=True \
-    +actor_rollout_ref.actor.optim.override_optimizer_config.optimizer_offload_fraction=1 \
-    +actor_rollout_ref.actor.optim.override_optimizer_config.overlap_cpu_optimizer_d2h_h2d=True \
-    +actor_rollout_ref.actor.optim.override_optimizer_config.use_precision_aware_optimizer=True \
-    +actor_rollout_ref.actor.optim.override_optimizer_config.optimizer_cpu_offload=True \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.moe_router_dtype=fp32 \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.moe_enable_deepep=True \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.moe_token_dispatcher_type=flex \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_method=uniform \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_granularity=full \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_num_layers=1 \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.gradient_accumulation_fusion=True \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.moe_permute_fusion=True \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=2 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.use_kl_in_reward=False \
     reward_model.reward_manager=dapo \
     +reward_model.reward_kwargs.overlong_buffer_cfg.enable=True \
@@ -81,4 +50,5 @@ CUDA_VISIBLE_DEVICES=0,3,4,7 python3 -m verl.trainer.main_ppo --config-path=conf
     trainer.nnodes=1 \
     trainer.save_freq=20 \
     trainer.test_freq=5 \
+    trainer.val_only=True \
     trainer.total_epochs=15 $@
